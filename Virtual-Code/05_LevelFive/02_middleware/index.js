06cb04cddbdba39e6cfd06b7d2b7f3d230645a63
Ankit Tripathi <ankittripathi2k24@gmail.com>
2026-05-22 19:37:10 +0530

    done

---
+++ Virtual-Code/05_LevelFive/02_middleware/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+// built-in middleware
+app.use(express.json());
+
+// middleware -logging, auth, validation
+// creation of custom middleware
+const loggingMiddleware = (req, res, next) => {
+  console.log("Logging Kra hu");
+  next();
+};
+// loading middlware inti application
+app.use(loggingMiddleware);
+
+const authMiddleware = (req, res, next) => {
+  console.log("auth Kra hu");
+  res.send("chalo seeda ghar");
+  // next();
+};
+app.use(authMiddleware);
+
+const validationMiddleware = (req, res, next) => {
+  console.log("validation Kra hu");
+  next();
+};
+app.use(validationMiddleware);
+
+app.get("/", (req, res) => {
+  console.log("Main route handler hu");
+  console.log(req.body);
+  res.send("hello World");
+});
+
+app.listen(port, () => {
+  console.log(`server running at ${port} ..`);
+});
